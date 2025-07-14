c1d0e8d056013162a024ddf273717cebdf905130
Dhruvi Tailor <dhruvitailor47@gmail.com>
2025-07-14 19:40:03 +0530

    details of student

---
+++ student_detail.py
@@ -0,0 +1,14 @@
+# This script defines a student's details and prints them.
+stud_name="Dhruvi"
+stud_age=18
+stud_height=5.8
+stud_department = "information technology"
+stud_id = "240841305015"
+stud_marks = "9 spi"
+
+print("Student's name:",stud_name)
+print("Student's age:",stud_age)
+print("Student's height:",stud_height)
+print("Student's department:",stud_department)
+print("Student's ID:",stud_id) 
+print("Student's marks:",stud_marks)
