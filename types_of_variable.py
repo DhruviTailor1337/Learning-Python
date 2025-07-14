341b66b408e32e303281e53e499085ce6fe1a31b
Dhruvi Tailor <dhruvitailor47@gmail.com>
2025-07-14 19:33:55 +0530

    diff

---
+++ types_of_variable.py
@@ -0,0 +1,13 @@
+# This script defines various types of variables and prints their types.
+name="Dhruvi"
+age=18
+height=5.8
+latter = "D"
+is_student = True
+is_employed = False
+print("name:",type(name))
+print("age:",type(age))
+print("height:",type(height))
+print("latter:",type(latter))
+print("is_student:",type(is_student))
+print("is_employed:",type(is_employed))
