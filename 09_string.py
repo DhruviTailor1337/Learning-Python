2e131f219c58aa2ab4ca3d9d298c99a5bd10ff38
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-07-16 07:41:52 +0530

    print last 3 char

---
+++ 09_string.py
@@ -0,0 +1,16 @@
+#create a string and print last 3 char 
+str1="Dhruvi"
+print(str1[3])
+print(str1[4])
+print(str1[5])
+
+#or we can use negataive indexing 
+print(str1[-3])
+print(str1[-2])
+print(str1[-1])
+
+#we can use slicing to print in single line
+print(str1[3:])
+
+#also we can slice like this:
+print(str1[-3:])
