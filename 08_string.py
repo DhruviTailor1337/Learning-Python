b6e95e81dcc474951471a92ea2cee1f6b0141c8c
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-07-16 07:31:07 +0530

    print random char from str

---
+++ 08_string.py
@@ -0,0 +1,9 @@
+# create a string and print first, fifth and last char.
+str1 = "DhruviTailor"
+print(str1[0])
+print(str1[4])
+print(str1[11])
+
+
+# or we can extract char with the help of string concatination
+print(str1[0]+" "+str1[4]+" "+str1[11])
