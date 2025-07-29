979b79eaff8e977359626e4dbfa5f478f13e99aa
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-07-29 19:40:59 +0530

    get type of variable

---
+++ 14_get_type.py
@@ -0,0 +1,15 @@
+#if we want to know what kind of data type a variable is holding we can use type() method
+
+a=10
+b=10.22
+c="D"        #in python char data type does not exists. chat in python also treated as string
+d="Dhruvi"
+e=True
+f=False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
