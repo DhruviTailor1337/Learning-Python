84fb8018bdaed4a0e6abb1aaf07f369a538c6614
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-16 10:45:50 +0530

    creating lists

---
+++ 18_List.py
@@ -0,0 +1,21 @@
+# list in python allow us to store multiple items in a single variable
+
+# empty list
+mylist = []
+print(mylist)
+
+# collection of roll numbers(integers)
+rollNum = [101,102,103,104,105]
+print(rollNum)
+
+# collection of names(Strings)
+names = ["Dhruvi", "Trisha","Megh"]
+print(names)
+
+# collection of marks(floats)
+marks = [91.89, 88.90, 71.00, 32.09]
+print(marks)
+
+# collection of all kind of data-type
+mixList = [101, "Dhruvi", 88.90 , True]
+print(mixList)
