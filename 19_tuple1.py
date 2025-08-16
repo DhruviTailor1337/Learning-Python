d527b7e43954db5760cb63cd795c07ad58c23736
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-16 11:18:16 +0530

    creating tuple

---
+++ 19_tuple1.py
@@ -0,0 +1,22 @@
+# tuple in python allow us to store multiple items in a single variable
+# but they are immutable.
+
+# empty tuple
+mytuple = ()
+print(mytuple)
+
+# collection of roll numbers(integers)
+rollNum = (101,102,103,104,105)
+print(rollNum)
+
+# collection of names(Strings)
+names = ("Dhruvi", "Trisha","Megh")
+print(names)
+
+# collection of marks(floats)
+marks = (91.89, 88.90, 71.00, 32.09)
+print(marks)
+
+# collection of all kind of data-type
+mixtup = (101, "Dhruvi", 88.90 , True)
+print(mixtup)
