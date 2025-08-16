c89ee4ba456c57bdabf6602cf39b4cd3b0c309b0
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-16 10:51:58 +0530

    change list items

---
+++ 18_List3.py
@@ -0,0 +1,11 @@
+# to change the value of a specific item, refer to the index number
+
+# change the secong item of the list
+list1 = ["apple", "ball" , "cat", "dog"]
+list1[1] = "bat"
+print(list1)
+
+# change the range of item value 
+list2 = ["apple", "ball" , "cat", "dog" , "elephant"]
+list2[0:3] = ["aeroplane", "bat", "cow"]
+print(list2)
