e9a24a6bbf538eed02317211baa3851b291dbb22
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-08-28 08:18:33 +0530

    week

---
+++ 26_match.py
@@ -0,0 +1,20 @@
+day = 2
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _ :        #default block
+        print("invalid number")
+         
+print("code executed successfully.")
