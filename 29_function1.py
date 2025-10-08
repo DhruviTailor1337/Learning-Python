346457b3d249ab1510ab7b891d54f3ee2fd325b1
DhruviTailor1337 <dhruvitailor47@gmail.com>
2025-10-08 09:35:05 +0530

    functions

---
+++ 29_function1.py
@@ -0,0 +1,16 @@
+#simple function
+def myFunct1():
+    print("Hello")
+myFunct1()
+
+# function with 1 argument
+def myFunct2(name):
+    print(name)
+myFunct2("Dhruvi")
+
+# functon with 2 argument
+def myFunct3(age,rollNum):
+    print("your age is:",age)
+    print("your roll number is:",rollNum)
+myFunct3(19,44)
+
