deb0737c78538a5f8c4567d39e5a0f7155c1103c
DhruviTailor1337 <dhruvitailor47@gmail.com>
2026-07-04 20:54:14 +0530

    different function

--- 29_function1.py
+++ 29_function1.py
@@ -14,3 +14,23 @@ def myFunct3(age,rollNum):
     print("your roll number is:",rollNum)
 myFunct3(19,44)
 
+#function with default argument
+def myFunct4(rollNum,age=20):       #the non default arguments always comes first
+    print(f'your age is:{age} and roll number is {rollNum}')
+
+myFunct4(44)
+
+
+#keyword arguments
+def myfunct5(rollnum,age):
+    print(f"your roll number is {rollnum} and age is {age}")
+myfunct5(age=25,rollnum=44)
+
+
+#lambda function to find square
+square = lambda x:x**2
+print(square(5))
+
+#lambda function to find even odd
+check = lambda x: 'even' if x%2==0 else 'odd'
+print(check(5))
